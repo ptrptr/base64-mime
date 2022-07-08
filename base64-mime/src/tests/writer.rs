d2b4bd583ebc9ab7c1f3eb9ae1790896e8f637ab
Petri Sarvamaa <ptrptrdev@gmail.com>
2022-07-08 12:04:11 +0300

    Implement empty write_all for writer

--- base64-mime/src/tests/writer.rs
+++ base64-mime/src/tests/writer.rs
@@ -1,7 +1,18 @@
+use std::io::Write;
+
 use crate::Base64Writer;
 
 #[test]
 fn test_new_writer() {
     let mut empty: &mut [u8] = &mut [0u8; 0];
     let _writer: Base64Writer = Base64Writer::new(&mut empty);
+}
+
+#[test]
+fn test_empty_write() -> std::io::Result<()> {
+    let mut empty: &mut [u8] = &mut [0u8; 0];
+    let mut writer: Base64Writer = Base64Writer::new(&mut empty);
+    let _ = writer.write_all("".as_bytes())?;
+    assert_eq!("".as_bytes(), empty, "writing empty should result in empty");
+    Ok(())
 }
--- base64-mime/src/writer.rs
+++ base64-mime/src/writer.rs
@@ -7,3 +7,13 @@ impl Base64Writer {
         Base64Writer {}
     }
 }
+
+impl Write for Base64Writer {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        todo!()
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        todo!()
+    }
+}
