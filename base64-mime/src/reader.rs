c9046ba5f915ff74681af184ac12592104ae3a7b
Petri Sarvamaa <ptrptrdev@gmail.com>
2022-07-09 11:01:55 +0300

    Implement reading non-padded content

--- base64-mime/src/reader.rs
+++ base64-mime/src/reader.rs
@@ -1,15 +1,70 @@
-use std::io::Read;
+use std::io::{BufRead, Read};
 
-pub struct Base64Reader {}
+pub struct Base64Reader<R: BufRead> {
+    reader: R,
+}
 
-impl Base64Reader {
-    pub fn new(reader: &dyn Read) -> Base64Reader {
-        Base64Reader {}
+impl<R> Base64Reader<R>
+where
+    R: BufRead,
+{
+    pub fn new(reader: R) -> Base64Reader<R> {
+        Base64Reader { reader }
     }
 }
 
-impl Read for Base64Reader {
+impl<R> Read for Base64Reader<R>
+where
+    R: BufRead,
+{
     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        Ok(0)
+        let maybe_word = self.read_word()?;
+        match maybe_word {
+            Some(word) => {
+                let mut destination = &mut buf[0..4];
+                decode_word(word, &mut destination);
+                Ok(3)
+            }
+            None => Ok(0),
+        }
+    }
+}
+
+impl<R> Base64Reader<R>
+where
+    R: BufRead,
+{
+    fn read_word(&mut self) -> std::io::Result<Option<[u8; 4]>> {
+        let mut result = [0u8; 4];
+        for i in 0..=3 {
+            let had_more = self.reader.read(&mut result[i..i + 1])? != 0;
+            if had_more {
+                continue;
+            }
+            if i == 0 {
+                return Ok(None);
+            } else {
+                todo!("Handle EOF within word boundary")
+            }
+        }
+        Ok(Some(result))
+    }
+}
+
+fn decode_word(word: [u8; 4], destination: &mut [u8]) {
+    let ordinals = word.map(decode_ordinal);
+    destination[0] = (ordinals[0] << 2) | (ordinals[1] >> 4);
+    destination[1] = (ordinals[1] << 4) | (ordinals[2] >> 2);
+    destination[2] = (ordinals[2] << 6) | ordinals[3];
+}
+
+fn decode_ordinal(ordinal: u8) -> u8 {
+    match ordinal as char {
+        'A'..='Z' => ordinal - 'A' as u8,
+        'a'..='z' => 26 + (ordinal - 'a' as u8),
+        '0'..='9' => 52 + (ordinal - '0' as u8),
+        '+' => 62,
+        '/' => 63,
+        _ => todo!("Handle bad characters"),
     }
 }
--- base64-mime/src/tests/reader.rs
+++ base64-mime/src/tests/reader.rs
@@ -4,15 +4,25 @@ use std::io::Read;
 #[test]
 fn test_new_reader() {
     let empty = String::new();
-    let _reader = Base64Reader::new(&empty.as_bytes());
+    let _reader = Base64Reader::new(empty.as_bytes());
 }
 
 #[test]
 fn test_empty_read_to_end() -> std::io::Result<()> {
     let empty: String = String::new();
-    let mut reader = Base64Reader::new(&empty.as_bytes());
+    let mut reader = Base64Reader::new(empty.as_bytes());
     let mut buf: Vec<u8> = Vec::new();
     reader.read_to_end(&mut buf)?;
     assert_eq!("".as_bytes(), buf, "empty should read as empty");
+    Ok(())
+}
+
+#[test]
+fn test_read_unpadded() -> std::io::Result<()> {
+    let mut reader = Base64Reader::new("Rm9v".as_bytes());
+    let mut buf: Vec<u8> = Vec::new();
+    let count = reader.read_to_end(&mut buf)?;
+    assert_eq!(3, count, "Should read 3 bytes");
+    assert_eq!("Foo".as_bytes(), buf, "\"Rm9v\" should read as \"Foo\"");
     Ok(())
 }
