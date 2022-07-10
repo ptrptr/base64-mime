3916f82e1f2ddfb0f3514559553c0fa2d1e8d4d3
Petri Sarvamaa <ptrptrdev@gmail.com>
2022-07-10 23:23:57 +0300

    Implement unpadded writes

--- base64-mime/src/tests/writer.rs
+++ base64-mime/src/tests/writer.rs
@@ -4,14 +4,14 @@ use crate::Base64Writer;
 
 #[test]
 fn test_new_writer() {
-    let mut empty: &mut [u8] = &mut [0u8; 0];
-    let _writer: Base64Writer = Base64Writer::new(&mut empty);
+    let empty: Vec<u8> = Vec::new();
+    let _writer = Base64Writer::new(empty);
 }
 
 #[test]
 fn test_empty_write_all() -> std::io::Result<()> {
-    let mut empty: &mut [u8] = &mut [0u8; 0];
-    let mut writer: Base64Writer = Base64Writer::new(&mut empty);
+    let mut empty: Vec<u8> = Vec::new();
+    let mut writer = Base64Writer::new(&mut empty);
     let _ = writer.write_all("".as_bytes())?;
     assert_eq!("".as_bytes(), empty, "writing empty should result in empty");
     Ok(())
@@ -20,8 +20,18 @@ fn test_empty_write_all() -> std::io::Result<()> {
 #[test]
 fn test_empty_write() -> std::io::Result<()> {
     let mut empty: &mut [u8] = &mut [0u8; 0];
-    let mut writer: Base64Writer = Base64Writer::new(&mut empty);
+    let mut writer = Base64Writer::new(&mut empty);
     let _ = writer.write("".as_bytes())?;
     assert_eq!("".as_bytes(), empty, "writing empty should result in empty");
+    Ok(())
+}
+
+#[test]
+fn test_write_unpadded() -> std::io::Result<()> {
+    let mut buffer: Vec<u8> = Vec::new();
+    let mut writer = Base64Writer::new(&mut buffer);
+    let count = writer.write("Foo".as_bytes())?;
+    assert_eq!(4, count, "3 input bytes should produce 4 output bytes");
+    assert_eq!("Rm9v".as_bytes(), buffer, "\"Foo\" should encode to \"Rm9v\"");
     Ok(())
 }
--- base64-mime/src/writer.rs
+++ base64-mime/src/writer.rs
@@ -1,23 +1,50 @@
 use std::io::Write;
 
-pub struct Base64Writer {}
+pub struct Base64Writer<W : Write> {
+    writer: W
+}
 
-impl Base64Writer {
-    pub fn new(writer: &dyn Write) -> Base64Writer {
-        Base64Writer {}
+impl<W> Base64Writer<W> where W : Write {
+    pub fn new(writer: W) -> Base64Writer<W> {
+        Base64Writer {writer}
     }
 }
 
-impl Write for Base64Writer {
+impl<W> Write for Base64Writer<W> where W : Write {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        if buf.len() == 0 {
-            Ok(0)
-        } else {
-            todo!()
+        match buf.len() {
+            0 =>    Ok(0),
+            1..=2 => todo!("handle small buffer"),
+            _ => {
+                let word = &buf[0..=2];
+                let encoded_word = encode_word(&word);
+                self.writer.write_all(&encoded_word[..])?;
+                Ok(4)
+            },
         }
     }
 
     fn flush(&mut self) -> std::io::Result<()> {
         todo!()
+    }
+}
+
+fn encode_word(buf: &[u8]) -> [u8;4] {
+    let mut ordinals = [0u8;4];
+    ordinals[0] = buf[0] >> 2;
+    ordinals[1] = ((buf[0] << 4) | (buf[1] >> 4)) & 0b00111111u8;
+    ordinals[2] = ((buf[1] << 2) | (buf[2] >> 6)) & 0b00111111u8;
+    ordinals[3] = buf[2] & 0b00111111u8;
+    ordinals.map(encode_symbol)
+}
+
+fn encode_symbol(ordinal: u8) -> u8 {
+    match ordinal {
+        0..=25 => 'A' as u8 + ordinal,
+        26..=51 => 'a' as u8 + (ordinal - 26),
+        52..=61 => '0' as u8 + (ordinal - 52),
+        62 => '+' as u8, 
+        63 => '/' as u8,
+        _ => panic!("should never happen"),
     }
 }
