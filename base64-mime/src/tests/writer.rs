062245da33556fbd0d3483b6bd7dac1cce1aa9c2
Petri Sarvamaa <ptrptrdev@gmail.com>
2022-07-11 13:54:01 +0300

    Use write_all in write tests

--- base64-mime/src/tests/writer.rs
+++ base64-mime/src/tests/writer.rs
@@ -9,11 +9,15 @@ fn test_new_writer() {
 }
 
 #[test]
-fn test_empty_write_all() -> std::io::Result<()> {
+fn test_empty_write_call() -> std::io::Result<()> {
     let mut empty: Vec<u8> = Vec::new();
     let mut writer = Base64Writer::new(&mut empty);
-    let _ = writer.write_all("".as_bytes())?;
-    assert_eq!("".as_bytes(), empty, "writing empty should result in empty");
+    let _ = writer.write("".as_bytes())?;
+    assert_eq!(
+        "".as_bytes(),
+        empty,
+        "calling write with empty string should not write anything"
+    );
     Ok(())
 }
 
@@ -77,7 +81,7 @@ fn template_write_test_with_text_and_expected(
 ) -> std::io::Result<()> {
     let mut buffer: Vec<u8> = Vec::new();
     let mut writer = Base64Writer::new(&mut buffer);
-    let _ = writer.write(text.as_bytes())?;
+    let _ = writer.write_all(text.as_bytes())?;
     writer.flush()?;
     assert_eq!(
         expected.len(),
