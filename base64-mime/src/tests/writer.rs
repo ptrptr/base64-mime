90f537725ccb349c26a80e229bf76cbb60606947
Petri Sarvamaa <ptrptrdev@gmail.com>
2022-07-11 13:19:00 +0300

    Simplify writer tests

--- base64-mime/src/tests/writer.rs
+++ base64-mime/src/tests/writer.rs
@@ -19,57 +19,45 @@ fn test_empty_write_all() -> std::io::Result<()> {
 
 #[test]
 fn test_empty_write() -> std::io::Result<()> {
-    let mut empty: &mut [u8] = &mut [0u8; 0];
-    let mut writer = Base64Writer::new(&mut empty);
-    let _ = writer.write("".as_bytes())?;
-    assert_eq!("".as_bytes(), empty, "writing empty should result in empty");
-    Ok(())
+    template_write_test_with_text_and_expected("", "")
 }
 
 #[test]
 fn test_write_unpadded() -> std::io::Result<()> {
-    let mut buffer: Vec<u8> = Vec::new();
-    let mut writer = Base64Writer::new(&mut buffer);
-    let count = writer.write("Foo".as_bytes())?;
-    assert_eq!(4, count, "3 input bytes should produce 4 output bytes");
-    assert_eq!(
-        "Rm9v".as_bytes(),
-        buffer,
-        "\"Foo\" should encode to \"Rm9v\""
-    );
-    Ok(())
+    template_write_test_with_text_and_expected("Foo", "Rm9v")
 }
 
 #[test]
 fn test_write_one_padding_byte() -> std::io::Result<()> {
-    let mut buffer: Vec<u8> = Vec::new();
-    let mut writer = Base64Writer::new(&mut buffer);
-    let _ = writer.write("Fo".as_bytes())?;
-    writer.flush()?;
-    assert_eq!(
-        4,
-        buffer.len(),
-        "2 input bytes should produce 4 output bytes"
-    );
-    assert_eq!(
-        "Rm8=".as_bytes(),
-        buffer,
-        "\"Fo\" should encode to \"Rm8=\""
-    );
-    Ok(())
+    template_write_test_with_text_and_expected("Fo", "Rm8=")
 }
 
 #[test]
 fn test_write_two_padding_bytes() -> std::io::Result<()> {
+    template_write_test_with_text_and_expected("F", "Rg==")
+}
+
+fn template_write_test_with_text_and_expected(
+    text: &'static str,
+    expected: &'static str,
+) -> std::io::Result<()> {
     let mut buffer: Vec<u8> = Vec::new();
     let mut writer = Base64Writer::new(&mut buffer);
-    let _ = writer.write("F".as_bytes())?;
+    let _ = writer.write(text.as_bytes())?;
     writer.flush()?;
     assert_eq!(
-        4,
+        expected.len(),
         buffer.len(),
-        "1 input bytes should produce 4 output bytes"
+        "{} input bytes should produce {} output bytes",
+        text.len(),
+        expected.len(),
     );
-    assert_eq!("Rg==".as_bytes(), buffer, "\"F\" should encode to \"Rg==\"");
+    assert_eq!(
+        expected.as_bytes(),
+        buffer,
+        "\"{}\" should encode to \"{}\"",
+        text,
+        expected
+    );
     Ok(())
 }
