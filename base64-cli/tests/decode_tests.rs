a8216d3ce42ff8dc9a3947d8c278948d89627566
Petri Sarvamaa <ptrptrdev@gmail.com>
2022-07-21 01:13:36 +0300

    Test encoding and decoding to a file

--- base64-cli/tests/decode_tests.rs
+++ base64-cli/tests/decode_tests.rs
@@ -7,11 +7,11 @@ mod common;
 
 #[test]
 fn integration_test_decode_file() -> std::io::Result<()> {
-    common::write_file("output2", "SGVsbG8gd29ybGQh".as_bytes())?;
+    common::write_file("encoded1", "SGVsbG8gd29ybGQh".as_bytes())?;
     let binary = env!("CARGO_BIN_EXE_base64-cli");
     let output = process::Command::new(binary)
         .arg("-d")
-        .arg(common::get_file("output2"))
+        .arg(common::get_file("encoded1"))
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .spawn()?
@@ -23,6 +23,36 @@ fn integration_test_decode_file() -> std::io::Result<()> {
         "Hello world!", text_output,
         "Program should decode \"{}\" into {}",
         "SGVsbG8gd29ybGQh", "Hello world!"
+    );
+    Ok(())
+}
+
+#[test]
+fn integration_test_decode_file_with_output_file() -> std::io::Result<()> {
+    common::write_file("encoded2", "SGVsbG8gd29ybGQh".as_bytes())?;
+    common::write_file("decoded_output2", "".as_bytes())?;
+    let output = std::fs::File::options()
+        .write(true)
+        .open(common::get_file("decoded_output2"))?;
+    let binary = env!("CARGO_BIN_EXE_base64-cli");
+    let output = process::Command::new(binary)
+        .arg("-d")
+        .arg(common::get_file("encoded2"))
+        .stdout(output)
+        .stderr(Stdio::piped())
+        .spawn()?
+        .wait_with_output()?;
+    let text_output = String::from_utf8(output.stdout).expect("should return valid utf-8");
+    let err_output = String::from_utf8(output.stderr).expect("stderr should return valid utf-8");
+    assert_eq!("", err_output, "Stderr should be empty");
+    assert_eq!("", text_output, "Stdout should be empty");
+    assert_eq!(
+        "Hello world!",
+        String::from_utf8(common::read_file("decoded_output2")?)
+            .expect("should decode valid utf-8"),
+        "Program should decode \"{}\" into {}",
+        "SGVsbG8gd29ybGQh",
+        "Hello world!"
     );
     Ok(())
 }
--- base64-cli/tests/encode_tests.rs
+++ base64-cli/tests/encode_tests.rs
@@ -7,10 +7,10 @@ mod common;
 
 #[test]
 fn integration_test_encode_file() -> std::io::Result<()> {
-    common::write_file("output1", "Hello world!".as_bytes())?;
+    common::write_file("decoded1", "Hello world!".as_bytes())?;
     let binary = env!("CARGO_BIN_EXE_base64-cli");
     let output = process::Command::new(binary)
-        .arg(common::get_file("output1"))
+        .arg(common::get_file("decoded1"))
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .spawn()?
@@ -22,6 +22,35 @@ fn integration_test_encode_file() -> std::io::Result<()> {
         "SGVsbG8gd29ybGQh", text_output,
         "Program should encode \"{}\" into {}",
         "Hello world!", "SGVsbG8gd29ybGQh"
+    );
+    Ok(())
+}
+
+#[test]
+fn integration_test_encode_file_with_output_file() -> std::io::Result<()> {
+    common::write_file("decoded2", "Hello world!".as_bytes())?;
+    common::write_file("encoded_output2", "".as_bytes())?;
+    let output = std::fs::File::options()
+        .write(true)
+        .open(common::get_file("encoded_output2"))?;
+    let binary = env!("CARGO_BIN_EXE_base64-cli");
+    let output = process::Command::new(binary)
+        .arg(common::get_file("decoded2"))
+        .stdout(output)
+        .stderr(Stdio::piped())
+        .spawn()?
+        .wait_with_output()?;
+    let text_output = String::from_utf8(output.stdout).expect("should return valid utf-8");
+    let err_output = String::from_utf8(output.stderr).expect("stderr should return valid utf-8");
+    assert_eq!("", err_output, "Stderr should be empty");
+    assert_eq!("", text_output, "Stdout should be empty");
+    assert_eq!(
+        "SGVsbG8gd29ybGQh",
+        String::from_utf8(common::read_file("encoded_output2")?)
+            .expect("should encode valid utf-8"),
+        "Program should encode \"{}\" into {}",
+        "Hello world!",
+        "SGVsbG8gd29ybGQh"
     );
     Ok(())
 }
